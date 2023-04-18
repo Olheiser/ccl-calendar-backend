b1abd5b4f1ad774c5111cf3a8135cfccb80c98b9
Tanner Olheiser <tolheiser@outlook.com>
2023-04-18 14:56:28 -0600

    Updated file with proper names

---
+++ router/courtAttendanceRoutes.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const courtAttendanceRouter = express.Router();
+const {
+    getAllCourtAttendances, 
+    getCourtAttendanceById, 
+    createCourtAttendance, 
+    updateCourtAttendance, 
+    deleteCourtAttendance,
+    getCourtAttendanceByUserId,
+    getCourtAttendanceByCity,
+    getCourtAttendancesByDate,
+    createCourtAttendanceWidget
+} = require('../controllers/courtAttendanceController');
+
+courtAttendanceRouter.route('/')
+  .get(getAllCourtAttendances)
+  .post(createCourtAttendance);
+
+courtAttendanceRouter.route('/widget')
+  .post(createCourtAttendanceWidget);
+
+courtAttendanceRouter.route('/:id')
+  .get(getCourtAttendanceById)
+  .patch(updateCourtAttendance)
+  .delete(deleteCourtAttendance);
+
+courtAttendanceRouter.route('/user/:user_id')
+  .get(getCourtAttendanceByUserId);
+
+courtAttendanceRouter.route('/city/:city')
+  .get(getCourtAttendanceByCity);
+
+courtAttendanceRouter.route('/courtSitting/:courtSitting_ID/attendanceDate/:year/:month/:day')
+  .get(getCourtAttendancesByDate);
+
+module.exports = courtAttendanceRouter;
